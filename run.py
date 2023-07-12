49db0075461cd0e3e87690a2f14f64f767dd80a6
Emma Brass <emma.8.brass@gsk.com>
2023-07-12 14:18:08 +0100

    added CLR control, added volumes tracking to heinsight

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-
+from pylabware import Optimax
 import os
 import cv2
 from heinsight.liquidlevel.liquid_level import LiquidLevel
@@ -15,6 +15,28 @@ from heinsight.liquidlevel.track_tolerance_levels import \
 # Things I could add to improve HeinSight:
 # Ability to also look at color and take that into account when deciding on the liquid level
 # Ability to read text labels stuck on the vessel (or even like the vol labels that come printed on some vessels) and find ref level from them
+clr = Optimax(device_name="radleys_clr", port="COMX", connection_mode="serial", address="00", experiment_name="test1")
+clr.simulation = False
+
+clr._test_exe_path()
+clr.initialize_device()
+
+clr._add_stirring_step(200,20)
+clr._add_dosing_step(54)
+
+# For running the whole process...
+
+# Initial step of user setting the desired volune levels
+# Save these as a dict
+# User sets order to assess... e.g. first step is wait until at 4 vols
+
+# Turn reactor on, heat to specified temp 
+# Stop stirring every 30 sec, take a picture, then resume stirring
+# Analyse pic and return distance from liquid level to ref level for current step
+# If at ref level, stop, otherwise continue
+# If at ref level, pump in x ml of next solvent
+# 
+
 
 tracker = TrackTwoLiquidToleranceLevels()
 
@@ -26,7 +48,7 @@ liquid_level = LiquidLevel(
     track_liquid_tolerance_levels=tracker,
     use_tolerance=False,
     use_reference=True,
-    rows_to_count=30,
+    rows_to_count=25,
     number_of_liquid_levels_to_find=1,
     find_meniscus_minimum=0.03,
     no_error=False,
@@ -35,9 +57,11 @@ liquid_level = LiquidLevel(
 
 
 liquid_level.start(image=image, select_region_of_interest=True, set_reference=True, 
-    no_reference_lines=3, select_tolerance=False)
+    volumes_list = [3.5, 4, 6], select_tolerance=False)
 
-liquid_level.run(image=image_for_run)
+liquid_level.run(image=image_for_run) # will return % diff from volumes lines at 3.5, 4 and 6 volumes in the order specified in the start method
+
+# run method should only take ONE reference level as input -> whereas start should be allowed to use several.
 
 
 
